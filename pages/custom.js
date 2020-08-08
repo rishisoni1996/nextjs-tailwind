2854c16130f988ccb713e42d7013ab5f8472b488
Rishi Soni <rishi.s@upsquare.in>
2020-08-08 11:53:15 +0530

    first commit

---
+++ pages/custom.js
@@ -0,0 +1,275 @@
+import React from 'react'
+
+export default function Custom() {
+    return (
+        <>
+            <header className="text-gray-700 body-font">
+                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+                        </svg>
+                        <span className="ml-3 text-xl">tailblocks</span>
+                    </a>
+                    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                        <a className="mr-5 hover:text-gray-900">First Link</a>
+                        <a className="mr-5 hover:text-gray-900">Second Link</a>
+                        <a className="mr-5 hover:text-gray-900">Third Link</a>
+                        <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+                    </nav>
+                    <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">Button
+                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7" />
+                        </svg>
+                    </button>
+                </div>
+            </header>
+
+            <section className="text-gray-700 body-font">
+                <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
+        <br className="hidden lg:inline-block" />readymade gluten
+      </h1>
+                        <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                        <div className="flex justify-center">
+                            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                            <button className="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">Button</button>
+                        </div>
+                    </div>
+                    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                        <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+                    </div>
+                </div>
+            </section>
+
+            <section className="text-gray-700 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-20">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+                    </div>
+                    <div className="flex flex-wrap -m-2">
+                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80" />
+                                <div className="flex-grow">
+                                    <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
+                                    <p className="text-gray-500">UI Designer</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84" />
+                                <div className="flex-grow">
+                                    <h2 className="text-gray-900 title-font font-medium">Henry Letham</h2>
+                                    <p className="text-gray-500">CTO</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88" />
+                                <div className="flex-grow">
+                                    <h2 className="text-gray-900 title-font font-medium">Oskar Blinde</h2>
+                                    <p className="text-gray-500">Founder</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/90x90" />
+                                <div className="flex-grow">
+                                    <h2 className="text-gray-900 title-font font-medium">John Doe</h2>
+                                    <p className="text-gray-500">DevOps</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/94x94" />
+                                <div className="flex-grow">
+                                    <h2 className="text-gray-900 title-font font-medium">Martin Eden</h2>
+                                    <p className="text-gray-500">Software Engineer</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/98x98" />
+                                <div className="flex-grow">
+                                    <h2 className="text-gray-900 title-font font-medium">Boris Kitua</h2>
+                                    <p className="text-gray-500">UX Researcher</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/100x90" />
+                                <div className="flex-grow">
+                                    <h2 className="text-gray-900 title-font font-medium">Atticus Finch</h2>
+                                    <p className="text-gray-500">QA Engineer</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/104x94" />
+                                <div className="flex-grow">
+                                    <h2 className="text-gray-900 title-font font-medium">Alper Kamu</h2>
+                                    <p className="text-gray-500">System</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/108x98" />
+                                <div className="flex-grow">
+                                    <h2 className="text-gray-900 title-font font-medium">Rodrigo Monchi</h2>
+                                    <p className="text-gray-500">Product Manager</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <section className="text-gray-700 body-font relative">
+                <div className="container px-5 py-24 mx-auto flex sm:flex-no-wrap flex-wrap">
+                    <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                        <iframe width="100%" height="100%" className="absolute inset-0" frameBorder={0} title="map" marginHeight={0} marginWidth={0} scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.4)' }} />
+                        <div className="bg-white relative flex flex-wrap py-6">
+                            <div className="lg:w-1/2 px-6">
+                                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm">ADDRESS</h2>
+                                <p className="leading-relaxed">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
+                            </div>
+                            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm">EMAIL</h2>
+                                <a className="text-indigo-500 leading-relaxed">example@email.com</a>
+                                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mt-4">PHONE</h2>
+                                <p className="leading-relaxed">123-456-7890</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                        <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+                        <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+                        <input className="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" placeholder="Name" type="text" />
+                        <input className="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" placeholder="Email" type="email" />
+                        <textarea className="bg-white rounded border border-gray-400 focus:outline-none h-32 focus:border-indigo-500 text-base px-4 py-2 mb-4 resize-none" placeholder="Message" defaultValue={""} />
+                        <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                        <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+                    </div>
+                </div>
+            </section>
+
+     <footer className="text-gray-700 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap md:text-left text-center order-first">
+      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav className="list-none mb-10">
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav className="list-none mb-10">
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav className="list-none mb-10">
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2>
+        <div className="flex xl:flex-no-wrap md:flex-no-wrap lg:flex-wrap flex-wrap justify-center md:justify-start">
+          <input className="w-40 sm:w-auto bg-gray-100 rounded xl:mr-4 lg:mr-0 sm:mr-4 mr-2 border border-gray-400 focus:outline-none focus:border-indigo-500 text-base py-2 px-4" placeholder="Placeholder" type="text" />
+          <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+        </div>
+        <p className="text-gray-500 text-sm mt-2 md:text-left text-center">Bitters chicharrones fanny pack
+          <br className="lg:block hidden" />waistcoat green juice
+        </p>
+      </div>
+    </div>
+  </div>
+  <div className="bg-gray-200">
+    <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
+      <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+        </svg>
+        <span className="ml-3 text-xl">tailblocks</span>
+      </a>
+      <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 tailblocks —
+        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+      </p>
+      <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+        <a className="text-gray-500">
+          <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+          </svg>
+        </a>
+        <a className="ml-3 text-gray-500">
+          <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+          </svg>
+        </a>
+        <a className="ml-3 text-gray-500">
+          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
+            <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
+            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+          </svg>
+        </a>
+        <a className="ml-3 text-gray-500">
+          <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={0} className="w-5 h-5" viewBox="0 0 24 24">
+            <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" />
+            <circle cx={4} cy={4} r={2} stroke="none" />
+          </svg>
+        </a>
+      </span>
+    </div>
+  </div>
+</footer>
+
+        </>
+    )
+}
---
+++ pages/new.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default function New() {
+    return(
+        <>
+          <div>      
+            <div className="bannerFondo bg-green-800	bg-left-top bg-auto bg-repeat-x" style={{backgroundImage: 'url(./img/continuartl_4.png)', height: 400}}>
+            </div>
+            <div className="-mt-64 ">
+                <div className="w-full text-center">
+                <p className="text-sm tracking-widest text-white">Subtitle</p>
+                <h1 className="font-bold text-5xl text-white">
+                    Title
+                </h1>
+                </div>
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 ">
+                <div className="p-2 sm:p-10 text-center cursor-pointer">
+                    <div className="py-16 max-w-sm rounded overflow-hidden shadow-lg hover:bg-white transition duration-500  bg-white">
+                    <div className="space-y-10">
+                        <i className="fa fa-spa" style={{fontSize: 48}} />
+                        <div className="px-6 py-4">
+                        <div className="space-y-5">
+                            <div className="font-bold text-xl mb-2">Spa</div>
+                            <p className="text-gray-700 text-base">
+                            Todo tipo de masajes y técnicas
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="p-2 sm:p-10 text-center cursor-pointer text-white"> 
+                    <div className="py-16 max-w-sm rounded overflow-hidden shadow-lg bg-orange-500 hover:bg-orange-600 transition duration-500">
+                    <div className="space-y-10">
+                        <i className="fa fa-head-side-mask" style={{fontSize: 48}} />
+                        <div className="px-6 py-4">
+                        <div className="space-y-5">
+                            <div className="font-bold text-xl mb-2">Bioseguridad</div>
+                            <p className="text-base">
+                            Altos estandares de bioseguridad
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="p-2 sm:p-10 text-center cursor-pointer translate-x-2">
+                    <div className="py-16 max-w-sm rounded overflow-hidden shadow-lg hover:bg-white transition duration-500 bg-white ">
+                    <div className="space-y-10">
+                        <i className="fa fa-swimmer" style={{fontSize: 48}} />
+                        <div className="px-6 py-4">
+                        <div className="space-y-5">
+                            <div className="font-bold text-xl mb-2">Piscina</div>
+                            <p className="text-gray-700 text-base">
+                            Piscina temperada
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </>
+    )
+}
